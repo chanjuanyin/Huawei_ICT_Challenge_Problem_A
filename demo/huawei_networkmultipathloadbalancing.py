585494d3baafbf27777867eb5183ab2c2d569881
sirsir23333 <your_email@youremail.com>
2024-01-20 20:58:18 +0800

    1.Created all three classes in message.py, which are now called UserRequest, UserMessage and UserSwitchStatsInfo. 2.Updated NodePlacceholder class for adding more attributes such as id, level, remaining buffer, remaining inbound and remaning outbound. 3. Still coding for the UserSwitchStatsInfo

--- demo/huawei_networkmultipathloadbalancing.py
+++ demo/huawei_networkmultipathloadbalancing.py
@@ -2,21 +2,42 @@ from typing import List, Tuple
 from solution_common.message import Message, Request, SwitchStatsInfo
 from solution_common.solution import Solution
 
+#this class serves for the node information 101 for each node
 class NodePlaceholder:
-    major_id = int
 
-    def __init__(self, buffer_size, incoming_bandwidth, outgoing_bandwidth):
+    def __init__(self, id, level,buffer_size, incoming_bandwidth, outgoing_bandwidth, 
+                 remaining_buffer, remaining_inbound, remaining_outbound):
         # Instance attributes
+        self.id = id
+        self.level=level
         self.buffer_size = buffer_size
         self.incoming_bandwidth = incoming_bandwidth
         self.outgoing_bandwidth = outgoing_bandwidth
-
+        self.remaining_buffer = remaining_buffer
+        self.remaining_inbound = remaining_inbound
+        self.remaining_outbound = remaining_outbound
+        
+    #yet to update
     def display_info(self):
-        print(f"Device Info - Buffer Size: {self.buffer_size}, "
-              f"Incoming Bandwidth: {self.incoming_bandwidth}, Outgoing Bandwidth: {self.outgoing_bandwidth}")
+        print(f"Device Id : {self.id},"
+              f"Device Level : {self.level} "
+              f"Device Info - Buffer Size: {self.buffer_size}, "
+              f"Incoming Bandwidth: {self.incoming_bandwidth}, Outgoing Bandwidth: {self.outgoing_bandwidth}"
+              f"Remaining_Buffer: {self.remaining_buffer}, Remaining_Inbound: {self.remaining_inbound}, Remaining_Outbound: {self.remaining_outbound}")
 
-#This class if for rewriting and defining all the classes in message.py
+#Below three classes if for rewriting and defining all the classes in message.py
+
+
+class UserRequest(Request):
+    def __init__(self, source_node_id=0, target_node_id=0, data_size=0, begin_time=0, request_id=0):
+        super().__init__(source_node_id, target_node_id, data_size, begin_time, request_id)
+
 class UserMessage(Message):
+    def __init__(self, from_id: int, to_id: int, target_node_id: int, request_id: int, message_id: int,
+                 request_begin_time: int):
+        super().__init__(from_id, to_id, target_node_id, request_id, message_id, request_begin_time)
+
+class UserSwitchStatsInfo(SwitchStatsInfo):
     """define SwtichsStatsInfo here or just create another class in this file
     what to do   1.  a controler view of SwitchStatsInfo, including
                         1.1 identifier for controller, maybe put -1
@@ -26,8 +47,11 @@ class UserMessage(Message):
                         2.1 identifier for normal nodes, maybe put -2
                         2.2 see class message last five attributes
     """
-    pass
-
+    #check if it is controler view
+    def Controller_SSI():
+        pass
+    def Node_SSI():
+        pass
 class UserSolution(Solution):
     
     def __init__(self, node_id: int, bw_in: int, bw_out: int, size: int, level: int, graph: List[List[int]],
@@ -52,8 +76,8 @@ class UserSolution(Solution):
 
 
     # 1. take result: List[Tuple[Message, bool]]
-    # 2. decode and update it to SwitchStatsInfo
-    # 3. 
+    # 2. decode and update it to SwitchStatsInfo, the SwicthStatsInfo should be coincides with message
+    # 3. return SwitchStatsInfo
     def next_round(self, result) -> SwitchStatsInfo:
         # Information communication between node
         
