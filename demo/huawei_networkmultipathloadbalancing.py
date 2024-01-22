0f9ed247f2fd1c2909abe2df18fbfe19bf365673
sirsir23333 <your_email@youremail.com>
2024-01-22 17:02:55 +0800

    1. Rerwite three classes(UserRequest, UserMessage and UserSwitchStatsInfo) to UserRequest. Basically now UserRequest change minor id to a minor id list. This means that we changes the original message instance to an integer inside a minor id list. 2. Inside the UserRequest there are two methods for insert/delete  messages in minor id list. 3. Updated SwitchStatsInfo comments.  4. Updated Initialization inside UserSolution so that we have a newspaper for everynode.  4. Updated tasks in comment for Joshua Shing to do (inside add_request_list)

--- demo/huawei_networkmultipathloadbalancing.py
+++ demo/huawei_networkmultipathloadbalancing.py
@@ -29,6 +29,20 @@ class NodePlaceholder:
 #Below three classes if for rewriting and defining all the classes in message.py
 
 
+class UserRequest:
+    def __init__(self, request_id: int, target_node_id: int, request_begin_time: int):
+        self.request_id: int = request_id # MajorId
+        self.target_node_id: int = target_node_id
+        self.request_begin_time: int = request_begin_time
+        self.message_id: List[int] = [] # MinorId
+    def insert_message(self, int_list: List[int]):
+        merged_list=list(set(int_list) | set(self.message_id))
+        self.message_id=merged_list
+        
+    def delete_message(self,int_list: List[int]):
+        deleted_list=list(set(self.message_id)-set(int_list))
+        self.message_id=deleted_list
+
 class UserRequest(Request):
     def __init__(self, source_node_id=0, target_node_id=0, data_size=0, begin_time=0, request_id=0):
         super().__init__(source_node_id, target_node_id, data_size, begin_time, request_id)
@@ -40,61 +54,76 @@ class UserMessage(Message):
                  request_begin_time: int):
         super().__init__(from_id, to_id, target_node_id, request_id, message_id, request_begin_time)
 
-class UserSwitchStatsInfo(SwitchStatsInfo):
-    """define SwtichsStatsInfo here or just create another class in this file
-    what to do   1.  a controler view of SwitchStatsInfo, including
-                        1.1 identifier for controller, index -1       bitsize=1
-                        1.2 all node remaining buffer                 bitsize=11
-                        1.3 all nodes remaining inbound               bitsize=10
-                        1.4 and all nodes remaining outbound          bitsize=8
-                                                                      total bitsize=30
-                                                                      
-                    2.  a normal node view of SwitchStatsInfo including 
-                        part A what and who have I sent last time slice
-                        2.1 identifier for normal nodes, from_node_id                 bitsize=8
-                        2.2 where the message will send to, to_node_id                bitsize=8
-                        2.3 final destination, target_node_id                         bitsize=8
-                        2.4 Request Id(Major Id) that I send last time slice          bitsize=13
-                        2.5 Request Size()
-                        2.6 Message Id(Minor Id) range that I send last time slice    bitsize=
-                        
-                        part B My status that is two time slice ago
-                        
-                        2.x maybe node_info for the first time slice
-    """
-    def __init__(self,node_id):
-        super().__init__()
-        self.node_id=node_id
-        
-    #this is to transform number to binary number, maybe we can put it outside
-    def bin_tran(self,number:int):
-        if number<0:
-            return bin(number)[3:]
-        return bin(number)[2:]
-    
-    #define what will be the SSI like for controller
-    def Controller_SSI(self):
-        # indentifier
-        self.info.append(self.bin_tran(-1))
-        # all node remaining buffer
-        # task: how to represent remaining buffer
 
-        #self.info.append
-        
-    def Node_SSI(self,node_id):
-        # identifier
-        self.info.append(self.bin_tran(self.node_id))
+
+"""define SwtichsStatsInfo here or just create another class in this file
+what to do   1.  a controler view of SwitchStatsInfo, including
+                    First node:
+                    1.1.1 identifier for controller, index 2555     bitsize=8
+                                                                    total bitsize=8
+                    Rest of nodes at t=j-2:
+                    1.2.1 all node remaining buffer                 bitsize=11
+                    1.2.2 all nodes remaining inbound               bitsize=10
+                    1.2.3 and all nodes remaining outbound          bitsize=8
+                                                                    total bitsize=29
+                                                                    
+                2.  a normal node view of SwitchStatsInfo at t=j including 
+                    part A what and who have I sent last time slice
+                    First one out of 256 integers:
+                    2.1.1 identify myself                                         bitsize=8
+                    2.1.2 remaining outbound  t=j                                 bitsize=8
+                    2.1.3 new request added into you    t=j                       bitsize=11
+                    
+                    Rest of 255 integers
+                    For successful messages at t=j:
+                    2.2.1 where the message will send to, to_node_id                            bitsize=8
+                    2.2.2 final destination, target_node_id                                     bitsize=8
+                    2.2.3 Request Id(Major Id) that I send                                      bitsize=13
+                    2.2.4 from minor id (7 bits), to minor id (7 bits), continue read? (1 bit)  bitsize=15 X number of messages
+                    
+                    For failure messages at t=j:
+                    2.3.1 where the message will send to, to_node_id                            bitsize=8
+                    2.2.2 255 to denote you failed                                              bitsize=8
+                    Just ignore:
+                    2.4.1 255 to denote please don't read                                       bitsize=8
+
+                        
+                    2.x maybe node_info for the first time slice
+"""
+
         
 class UserSolution(Solution):
     
     def __init__(self, node_id: int, bw_in: int, bw_out: int, size: int, level: int, graph: List[List[int]],
                  nodes_info: List[Tuple[int, int, int, int, int]]):
         super().__init__(node_id, bw_in, bw_out, size, level, graph, nodes_info)
-        self.SwitchStatsInfo = UserSwitchStatsInfo(node_id)
+        nodes_info = sorted(nodes_info, key=lambda x: x[0])
+        if nodes_info[0][0]==-1:
+            nodes_info = nodes_info[1:]
+        self.node_info_update_newspaper = []
+        for i in range(len(graph[0])):
+            new_node = NodePlaceholder(nodes_info[i][0], nodes_info[i][1], nodes_info[i][4], nodes_info[i][2], nodes_info[i][3], 
+                 nodes_info[i][4], nodes_info[i][2], nodes_info[i][3])
+            self.node_info_update_newspaper.append(new_node)
+        self.requests_messages_you_possess = {} # {request_id: UserRequest object}
+        # Shing's work
+        self.remaining_outbound_of_myself = self.bw_out
+        self.remaining_buffer_of_myself = self.size
+        
         
     #the access node will receive request and divide into messages   
     def add_request_list(self, request_list: List[Request]) -> None:
-        # Remember to update your buffer
+        # Iterate through every request in List[Request]
+        # # For each request, first check if self.remaining_buffer_of_myself >= new request's size 
+        # If <, then ignore this entire request. Yes this will be detrimental to our success rate (hence our final score), but bopian
+        # If >=, then you can add the new request
+        #   How to add? You first create an object
+        #   user_request_object = UserRequest(a bunch of parameters you input for yourself)
+        #   then, you will do:
+        #   self.requests_messages_you_possess[your new request id that you should know] = user_request_object
+        #   last step, you will need to update your buffer by doing:
+        #   self.remaining_buffer_of_myself -= new request's size 
+        #   Everything in this indentation is within the >= logic of the if-else statement
         pass
 
 
@@ -106,9 +135,6 @@ class UserSolution(Solution):
         
         
         return []
-
-
-
         
         
     # 1. take result: List[Tuple[Message, bool]]
@@ -132,7 +158,27 @@ class UserSolution(Solution):
         
         #other nodes
         else:
-            
+            pass
             
         return SwitchStatsInfo()
+    
+        
+    #this is to transform number to binary number, maybe we can put it outside
+    def bin_tran(self,number:int):
+        if number<0:
+            return bin(number)[3:]
+        return bin(number)[2:]
+    
+    #define what will be the SSI like for controller
+    def Controller_SSI(self):
+        # indentifier
+        self.info.append(self.bin_tran(-1))
+        # all node remaining buffer
+        # task: how to represent remaining buffer
+
+        #self.info.append
+        
+    def Node_SSI(self,node_id):
+        # identifier
+        self.info.append(self.bin_tran(self.node_id))
 
